f92203f5a4a30914479c6c860ae5d0092ed517e2
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-14 16:23:28 -0500

    fix

--- zipx/src/sbt-test/splice-zipx/pins/project/ZipxVersions.scala
+++ zipx/src/sbt-test/splice-zipx/pins/project/ZipxVersions.scala
@@ -1,8 +1,15 @@
 import zipx.*
 
 object MyVersions extends ZipxVersions:
-  val sbt: SbtVersion     = SbtVersion("2.0.6")
+  val sbt: SbtVersion     = SbtVersion("2.1.0-M1")
   val scala: ScalaVersion = ScalaVersion("3.8.4")
   val scalafmt            = Plugin("org.scalameta", "sbt-scalafmt", "2.6.2")
   val scalajs             = Plugin("org.scala-js", "sbt-scalajs", "1.22.0")
-  val foo                 = Pin("splice", "foo", "1.0.0", sha256 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", purl = "pkg:npm/foo@1.0.0")
+  val foo                 = Pin(
+    "splice",
+    "foo",
+    "1.0.0",
+    sha256 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    purl = "pkg:npm/foo@1.0.0",
+  )
+end MyVersions
